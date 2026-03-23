84af174bf11614fa3201eb9dab090082f1989714
Ernest Ajieh <97366679+Ajiehnx@users.noreply.github.com>
2026-03-23 00:12:38 +0000

    Update Index.tsx

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,24 +1,3 @@
-import { Layout } from "@/components/layout/Layout";
-import { HeroSection } from "@/components/home/HeroSection";
-import { ValueProposition } from "@/components/home/ValueProposition";
-import { FeaturedProjects } from "@/components/home/FeaturedProjects";
-import { SkillsSnapshot } from "@/components/home/SkillsSnapshot";
-import { CertificationsStrip } from "@/components/home/CertificationsStrip";
-import { CTASection } from "@/components/home/CTASection";
-
-const Index = () => {
-  return (
-    <Layout>
-      <HeroSection />
-      <ValueProposition />
-      <FeaturedProjects />
-      <SkillsSnapshot />
-      <CertificationsStrip />
-      <CTASection />
-    </Layout>
-  );
-};
-
 export default function Index() {
   return (
     <main style={{ padding: '40px', fontFamily: 'Arial, sans-serif' }}>
